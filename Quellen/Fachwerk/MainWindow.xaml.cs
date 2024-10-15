0248d3ce3e9da234c02ab5881f51d121248a16bf
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-10-15 21:55:20 +0200

    add truss example

--- Quellen/Fachwerk/MainWindow.xaml.cs
+++ Quellen/Fachwerk/MainWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Windows;
+﻿using MathNet.Numerics.LinearAlgebra;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Fachwerk
 {
@@ -19,7 +26,7 @@ namespace Fachwerk
 
         public Node AddNode(string name, double x, double y)
         {
-            var node = new Node(this, name, x, y);
+            var node = new Node(this, Nodes.Count, name, x, y);
 
             Nodes.Add(node);
 
@@ -28,7 +35,7 @@ namespace Fachwerk
 
         public Rod AddRod(Node nodeA, Node nodeB)
         {
-            var rod = new Rod(this, nodeA, nodeB);
+            var rod = new Rod(this, Rods.Count, nodeA, nodeB);
 
             Rods.Add(rod);
 
@@ -37,7 +44,7 @@ namespace Fachwerk
 
         public Bearing AddBearing(Node node, bool fixX, bool fixY)
         {
-            var bearing = new Bearing(this, node, fixX, fixY);
+            var bearing = new Bearing(this, Bearings.Count, node, fixX, fixY);
 
             Bearings.Add(bearing);
 
@@ -46,7 +53,7 @@ namespace Fachwerk
 
         public ExternalForce AddExternalForce(Node node, double x, double y)
         {
-            var force = new ExternalForce(this, node, x, y);
+            var force = new ExternalForce(this, ExternalForces.Count, node, x, y);
 
             ExternalForces.Add(force);
 
@@ -55,7 +62,83 @@ namespace Fachwerk
 
         public void Solve()
         {
-            // TODO!
+            // Build matrix
+            var A = Matrix<double>.Build.Dense(Nodes.Count * 2, Rods.Count + Bearings.Count * 2);
+            foreach (var rod in Rods)
+            {
+                double dx = rod.NodeB.X - rod.NodeA.X;
+                double dy = rod.NodeB.Y - rod.NodeA.Y;
+
+                double l = Math.Sqrt(dx * dx + dy * dy);
+
+                A[rod.NodeA.Index * 2 + 0, rod.Index] = dx / l;
+                A[rod.NodeA.Index * 2 + 1, rod.Index] = dy / l;
+
+                A[rod.NodeB.Index * 2 + 0, rod.Index] = -dx / l;
+                A[rod.NodeB.Index * 2 + 1, rod.Index] = -dy / l;
+            }
+            foreach (var bearing in Bearings)
+            {
+                A[bearing.Node.Index * 2 + 0, Rods.Count + bearing.Index * 2 + 0] = bearing.FixX ? 1 : 0;
+                A[bearing.Node.Index * 2 + 1, Rods.Count + bearing.Index * 2 + 1] = bearing.FixY ? 1 : 0;
+            }
+
+            // Build node forces vector
+            var b = Vector<double>.Build.Dense(Nodes.Count * 2);
+            foreach (var externalForce in ExternalForces)
+            {
+                b[externalForce.Node.Index * 2 + 0] = externalForce.X;
+                b[externalForce.Node.Index * 2 + 1] = externalForce.Y;
+            }
+
+            // Calculate rod and bearing forces
+            var x = A.Svd().Solve(b);
+            foreach (var rod in Rods)
+            {
+                rod.Force = x[rod.Index];
+            }
+            foreach (var bearing in Bearings)
+            {
+                bearing.ForceX = x[Rods.Count + bearing.Index * 2 + 0];
+                bearing.ForceY = x[Rods.Count + bearing.Index * 2 + 1];
+            }
+
+            // Check solution for correctness
+            var diff = (A.Multiply(x) - b).L2Norm();
+            if (diff > 0.0001)
+            {
+                throw new Exception("Solution does not seem to be correct!");
+            }
+        }
+
+        public double GetMinX()
+        {
+            return Nodes.Min(node => node.X);
+        }
+
+        public double GetMaxX()
+        {
+            return Nodes.Max(node => node.X);
+        }
+
+        public double GetMinY()
+        {
+            return Nodes.Min(node => node.Y);
+        }
+
+        public double GetMaxY()
+        {
+            return Nodes.Max(node => node.Y);
+        }
+
+        public double GetMinForce()
+        {
+            return Rods.Min(rod => rod.Force);
+        }
+
+        public double GetMaxForce()
+        {
+            return Rods.Max(rod => rod.Force);
         }
 
         public override string ToString()
@@ -70,12 +153,16 @@ namespace Fachwerk
 
         public string Name { get; }
 
+        public int Index { get; }
+
         public double X { get; }
         public double Y { get; }
 
-        public Node(Model model, string name, double x, double y)
+        public Node(Model model, int index, string name, double x, double y)
         {
             Model = model;
+
+            Index = index;
 
             Name = name;
 
@@ -93,14 +180,18 @@ namespace Fachwerk
     {
         public Model Model { get; }
 
+        public int Index { get; }
+
         public Node NodeA { get; }
         public Node NodeB { get; }
 
         public double Force { get; set; } // To be computed!
 
-        public Rod(Model model, Node nodeA, Node nodeB)
+        public Rod(Model model, int index, Node nodeA, Node nodeB)
         {
             Model = model;
+
+            Index = index;
 
             NodeA = nodeA;
             NodeB = nodeB;
@@ -111,6 +202,8 @@ namespace Fachwerk
     {
         public Model Model { get; }
 
+        public int Index { get; }
+
         public Node Node { get; }
 
         public bool FixX { get; }
@@ -119,9 +212,11 @@ namespace Fachwerk
         public double ForceX { get; set; } // To be computed!
         public double ForceY { get; set; } // To be computed!
 
-        public Bearing(Model model, Node node, bool fixX, bool fixY)
+        public Bearing(Model model, int index, Node node, bool fixX, bool fixY)
         {
             Model = model;
+
+            Index = index;
 
             Node = node;
 
@@ -134,14 +229,18 @@ namespace Fachwerk
     {
         public Model Model { get; }
 
+        public int Index { get; }
+
         public Node Node { get; }
 
         public double X { get; }
         public double Y { get; }
 
-        public ExternalForce(Model model, Node node, double x, double y)
+        public ExternalForce(Model model, int index, Node node, double x, double y)
         {
             Model = model;
+
+            Index = index;
 
             Node = node;
 
@@ -163,14 +262,14 @@ namespace Fachwerk
 
             // Nodes
 
-            var a = model.AddNode("a", 0, 0);
-            var b = model.AddNode("b", 2, 0);
-            var c = model.AddNode("c", 4, 0);
-            var d = model.AddNode("d", 6, 0);
+            var a = model.AddNode("a",  0,  0);
+            var b = model.AddNode("b", 20,  0);
+            var c = model.AddNode("c", 40,  0);
+            var d = model.AddNode("d", 60,  0);
 
-            var e = model.AddNode("e", 1, 2);
-            var f = model.AddNode("f", 3, 2);
-            var g = model.AddNode("g", 5, 2);
+            var e = model.AddNode("e", 10, 20);
+            var f = model.AddNode("f", 30, 20);
+            var g = model.AddNode("g", 50, 20);
 
             // Rods
 
@@ -195,8 +294,8 @@ namespace Fachwerk
 
             // Forces
 
-            model.AddExternalForce(f, 0, 2);
-            model.AddExternalForce(g, 1, 0);
+            model.AddExternalForce(f, 0, 10);
+            model.AddExternalForce(g, 10, 0);
 
             // Calculate rod and bearing forces
 
@@ -209,13 +308,113 @@ namespace Fachwerk
             BearingDataGrid.ItemsSource = model.Bearings;
             ExternalForceDataGrid.ItemsSource = model.ExternalForces;
 
-            // WpfPlot
+            // Canvas
 
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 9, 16, 25 };
+            var minX = model.GetMinX();
+            var minY = model.GetMinY();
 
-            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
-            WpfPlot1.Refresh();
+            var maxX = model.GetMaxX();
+            var maxY = model.GetMaxY();
+
+            var innerWidth = maxX - minX;
+            var innerHeight = maxY - minY;
+
+            var pad = Math.Min(innerWidth, innerHeight) * 0.2;
+
+            var width = innerWidth + pad * 2;
+            var height = innerHeight + pad * 2;
+
+            var minForce = model.GetMinForce();
+            var maxForce = model.GetMaxForce();
+
+            var forceRange = maxForce - minForce;
+
+            DrawCanvas.Width = width;
+            DrawCanvas.Height = height;
+
+            foreach (var rod in model.Rods)
+            {
+                var p = (rod.Force - minForce) / forceRange;
+
+                var red = (1 - p) * 128 + 127;
+                var green = rod.Force < 0 ? (1 - rod.Force / minForce) * 128 + 127 : (1 - rod.Force / maxForce) * 128 + 127;
+                var blue = p * 128 + 127;
+
+                var color = System.Windows.Media.Color.FromRgb((byte) red, (byte) green, (byte) blue);
+                var brush = new SolidColorBrush(color);
+
+                var cx = (rod.NodeA.X + rod.NodeB.X) / 2;
+                var cy = (rod.NodeA.Y + rod.NodeB.Y) / 2;
+
+                var line = new Line();
+
+                line.Stroke = brush;
+                line.StrokeThickness = 2;
+                
+                line.X1 = pad + rod.NodeA.X;
+                line.Y1 = height - pad - rod.NodeA.Y;
+
+                line.X2 = pad + rod.NodeB.X;
+                line.Y2 = height - pad - rod.NodeB.Y;
+
+                DrawCanvas.Children.Add(line);
+
+                var text = new TextBlock();
+
+                text.Text = String.Format("{0:0.00}", rod.Force);
+                text.FontSize = 2;
+
+                var size = MeasureString(text);
+
+                text.SetValue(Canvas.LeftProperty, pad + cx - size.Width / 2);
+                text.SetValue(Canvas.TopProperty, height - pad - cy - size.Height / 2);
+
+                DrawCanvas.Children.Add(text);
+            }
+
+            foreach (var node in model.Nodes)
+            {
+                var circle = new Ellipse();
+
+                circle.Fill = Brushes.Black;
+
+                circle.Width = 4;
+                circle.Height = 4;
+
+                circle.SetValue(Canvas.LeftProperty, pad + node.X - 2);
+                circle.SetValue(Canvas.TopProperty, height - pad - node.Y - 2);
+
+                DrawCanvas.Children.Add(circle);
+
+                var text = new TextBlock();
+
+                text.Text = node.Name;
+                text.Foreground = Brushes.White;
+                text.FontSize = 2;
+
+                var size = MeasureString(text);
+
+                text.SetValue(Canvas.LeftProperty, pad + node.X - size.Width / 2);
+                text.SetValue(Canvas.TopProperty, height - pad - node.Y - size.Height / 2);
+
+                DrawCanvas.Children.Add(text);
+            }
+        }
+
+        static System.Windows.Size MeasureString(TextBlock block)
+        {
+            var textToFormat = block.Text;
+            var culture = CultureInfo.CurrentCulture;
+            var flowDirection = FlowDirection.LeftToRight;
+            var typeface = new Typeface(block.FontFamily, block.FontStyle, block.FontWeight, block.FontStretch);
+            var fontSize = block.FontSize;
+            var foreground = Brushes.Black;
+            var numberSubstitution = new NumberSubstitution();
+            var pixelPerDip = VisualTreeHelper.GetDpi(block).PixelsPerDip;
+
+            var formattedText = new FormattedText(textToFormat, culture, flowDirection, typeface, fontSize, foreground, numberSubstitution, pixelPerDip);
+
+            return new System.Windows.Size(formattedText.Width, formattedText.Height);
         }
     }
 }
--- README.md
+++ README.md
@@ -8,40 +8,85 @@ Dieses Repository enthält Beispiele zum Thema Computer-Simulation.
 
 ## 1. Modelle
 
-In diesem Kurs betrachten wir zwei Arten von Modellen, die sich in der Modellierung der Zeit unterscheiden:
+Grundsätzlich kann man zwischen statischen und dynamischen Modellen unterscheiden:
 
-* Zeitkontinuierliche Modelle
-* Zeitdiskrete Modelle
+* Statische Modelle (Betrachtung eines einzelnen stabilen Systemzustands)
+* Dynamische Modelle (Betrachtung der Änderung des Systemzustands über die Zeit)
 
-### 1.1. Zeitkontinuierliche Modelle
+### 1.1. Statische Modelle
+
+Statische Modelle betrachten Zustände, bei denen es ohne externe Einwirkung keine Zustandsänderung gibt.
+Bei solchen Modellen sind typischerweise einige Zustandseigenschaften bekannt, andere jedoch nicht.
+Des Weiteren beschreibt das Modell den Zusammenhang zwischen den bekannten und unbekannten Zustandseigenschaften.
+Simulationsprogramme sind dann dafür verantwortlich, die unbekannten Zustandseigenschaften zu berechnen.
+Wir betrachten die folgenden Beispiele, um ein besseres Verständnis für statische Modelle und deren Berechnung zu bekommen:
+
+* Fachwerk
+
+#### [Fachwerk](./Quellen/Fachwerk/)
+
+Ein Fachwerk ist ein System bestehend aus Knoten, die über Stäbe miteinander verbunden sind.
+Des Weiteren sind einige der Knoten gelagert, d.h. deren Position im Raum ist fixiert.
+Dabei können entweder beide Richtungungen oder nur eine der beiden Richtungen fixiert sein.
+Schließlich wirken auf die Knoten noch externe Kräft in einer oder zwei Richtungen.
+Das Computerprogramm berechnet die Kräfte, die auf die einzelne Stäbe wirken.
+Dabei kann man zwischen grundsätzlich Zugkräften und Druckkräften unterscheiden.
+Druckkräfte sind meist kritischer, da Stäbe in der Regel unter Druck leichter knicken als unter Zug zu brechen.
+Außerdem berechnet das Programm die Kräfte, die auf die unterschiedlichen Lager wirken.
+
+Zur Berechnung der Kräfte muss ein lineares Gleichungssystem gelöst werden.
+Für die Lösung des Gleichungssystems kann z.B. die Sinulärwertzerlegung verwendet werden.
+Die Singulärwertzerlegung berechnet eine Lösung mit der Methode der kleinsten Quadrate.
+Das Programm verwendet dafür die Bibliothek [Math.NET Numerics](https://numerics.mathdotnet.com/).
+
+![](./Quellen/Fachwerk/Screenshot.png)
+
+Die folgende Grafik zeigt das Datenmodell des Programms für die Berechnung der Lager- und Stabkräfte eines einfachen zweidimensionalen Fachwerks.
+Über die Klasse ``Model`` können Fachwerke inklusive der darin enthaltenen Knoten, Stäbe, Lager, und externen Kräfte definiert werden.
+Des Weiteren bietet die Klasse ``Model`` die Methode ``Solve``, welche mittels der Singulärwertzerlegung die Stab- und Lagerkräfte berechnet.
+Die Visualisierung erfolgt schließlich mit einem ``DataGrid`` sowie einem ``Canvas``, welche die Windows Presentation Foundation bereitstellt.
+
+![](./Quellen/Fachwerk/DataModel.svg)
+
+### 1.2. Dynamische Modelle
+
+Dynamische Modelle betrachten *nicht* einen einzelen stabilen Systemzustand, sondern die Änderung des Systemzustands über die Zeit.
+Dafür muss in der Regel ein Startzustand sowie eine Zustandsübergangsfunktion gegeben sein.
+Die Simulation rechnet dann den Zustand des Systems gemäß der Zustandsübergangsfunktion weiter.
+Man kann grundsätzlich zwischen zwei Arten von Modellen unterschieden werden:
+
+* Zeitkontinuierliche Modelle (Modell beschreibt Zustand zu jedem Zeitpunkt)
+* Zeitdiskrete Modelle (Modell beschreibt Zustands nur zu ausgewählten Zeitpunkten)
+
+#### 1.2.1. Zeitkontinuierliche Modelle
 
 Bei den zeitkontinuierlichen Modellen betrachten wir zwei Beispiele, die sich in ihrer Komplexität leicht unterscheiden und für welche die analytischen Lösungen bereits bekannt sind:
 
 * Ballwurf
 * Federpendel
 
-#### 1.1.1. [Ballwurf](./Quellen/SchieferWurf/)
+##### [Ballwurf](./Quellen/Ballwurf/)
 
 Dieses Beispiel zeigt die **numerische Integration zeitkontinuierlicher Modelle** mit dem expliziten und dem impliziten Euler-Verfahren sowie den Vergleich der numerischen Lösungen mit der analytischen Lösung, welche für dieses einfache Integral noch berechnet werden kann.
 
-![](./Quellen/SchieferWurf/Screenshot.png)
+![](./Quellen/Ballwurf/Screenshot.png)
 
-#### 1.1.2. [Federpendel](./Quellen/Federpendel/)
+##### [Federpendel](./Quellen/Federpendel/)
 
 *Kommt demnächst.*
 
-### 1.2. Zeitdiskrete Modelle
+#### 1.2.2. Zeitdiskrete Modelle
 
 Bei den zeitdiskreten Modellen können wieder zwei Arten unterschieden werden, die diskreten Zeitschritte durchzuführen:
 
 * Fester Zeitschritt
 * Variabler Zeitschritt
 
-#### 1.2.1. Fester Zeitschritt
+##### 1.2.2.1. Fester Zeitschritt
 
 *Kommt demnächst.*
 
-#### 1.2.2. Variabler Zeitschritt
+##### 1.2.2.2. Variabler Zeitschritt
 
 *Kommt demnächst.*
 
