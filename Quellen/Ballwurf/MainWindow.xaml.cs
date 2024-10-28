7984307cca21606f9879e0d0841e2a9cd75f89af
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-10-28 15:05:21 +0100

    work on ball plot

--- Quellen/Ballwurf/MainWindow.xaml.cs
+++ Quellen/Ballwurf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using ScottPlot;
+using System.Windows;
 
 namespace Ballwurf
 {
@@ -17,7 +18,7 @@ namespace Ballwurf
             var dt = 0.2; // in s
 
             var t0 = 0; // in s
-            var tmax = 2.3; // in s
+            var tmax = 2.5; // in s
 
             var steps = (int) ((tmax - t0) / dt) + 1;
 
@@ -67,15 +68,57 @@ namespace Ballwurf
 
             // Daten visualisieren
 
-            Visualization.Plot.Add.Scatter(dataX, dataAY);
-            Visualization.Plot.Add.Scatter(dataX, dataVY);
-            Visualization.Plot.Add.Scatter(dataX, dataPY);
-            
-            Visualization.Plot.Add.Scatter(dataX, dataVYE);
-            Visualization.Plot.Add.Scatter(dataX, dataPYE);
+            var start = Visualization.Plot.Add.VerticalLine(0);
+            start.LegendText = "Startzeitpunkt";
 
-            Visualization.Plot.Add.Scatter(dataX, dataVYI);
-            Visualization.Plot.Add.Scatter(dataX, dataPYI);
+            var ground = Visualization.Plot.Add.HorizontalLine(0);
+            ground.LegendText = "Boden";
+
+            var a = Visualization.Plot.Add.Scatter(dataX, dataAY);
+            a.LegendText = "Beschleunigung (in m/s²)";
+            a.LineColor = Color.FromHSL(0.25f, 1, 0.33f);
+            a.MarkerColor = Color.FromHSL(0.25f, 1, 0.33f);
+
+            var v = Visualization.Plot.Add.Scatter(dataX, dataVY);
+            v.LegendText = "Geschwindigkeit Analytisch (in m/s)";
+            v.LineWidth = 5;
+            v.LineColor = Color.FromHSL(0, 1, 0.25f);
+            v.MarkerSize = 14;
+            v.MarkerColor = Color.FromHSL(0, 1, 0.25f);
+
+            var ve = Visualization.Plot.Add.Scatter(dataX, dataVYE);
+            ve.LegendText = "Geschwindigkeit Numerisch Euler Explizit (in m/s)";
+            ve.LineWidth = 3;
+            ve.LineColor = Color.FromHSL(0, 1, 0.5f);
+            ve.MarkerSize = 10;
+            ve.MarkerColor = Color.FromHSL(0, 1, 0.5f);
+
+            var vi = Visualization.Plot.Add.Scatter(dataX, dataVYI);
+            vi.LegendText = "Geschwindigkeit Numerisch Euler Implizit (in m/s)";
+            vi.LineWidth = 1;
+            vi.LineColor = Color.FromHSL(0, 1, 0.75f);
+            vi.MarkerSize = 5;
+            vi.MarkerColor = Color.FromHSL(0, 1, 0.75f);
+
+            var p = Visualization.Plot.Add.Scatter(dataX, dataPY);
+            p.LegendText = "Position Analytisch (in m)";
+            p.LineColor = Color.FromHSL(0.75f, 1, 0.25f);
+            p.MarkerColor = Color.FromHSL(0.75f, 1, 0.25f);
+
+            var pe = Visualization.Plot.Add.Scatter(dataX, dataPYE);
+            pe.LegendText = "Position Numerisch Euler Explizit (in m)";
+            pe.LineColor = Color.FromHSL(0.75f, 1, 0.5f);
+            pe.MarkerColor = Color.FromHSL(0.75f, 1, 0.5f);
+
+            var pi = Visualization.Plot.Add.Scatter(dataX, dataPYI);
+            pi.LegendText = "Position Numerisch Euler Implizit (in m)";
+            pi.LineColor = Color.FromHSL(0.75f, 1, 0.75f);
+            pi.MarkerColor = Color.FromHSL(0.75f, 1, 0.75f);
+
+            Visualization.Plot.XLabel("Zeit (in s)");
+
+            Visualization.Plot.ShowLegend(Edge.Right);
+            Visualization.Plot.Legend.Alignment = Alignment.LowerLeft;
 
             Visualization.Refresh();
         }
