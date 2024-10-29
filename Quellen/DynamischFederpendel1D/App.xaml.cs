0a3916f715222285222ee8833fb16f016824b9c4
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-10-29 09:00:45 +0100

    add new project for spring system

---
+++ Quellen/DynamischFederpendel1D/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace DynamischFederpendel1D
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ Quellen/DynamischFederpendel1D/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DynamischFederpendel1D
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
