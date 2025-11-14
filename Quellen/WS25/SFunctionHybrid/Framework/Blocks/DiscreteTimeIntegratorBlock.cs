b89557f95de08cfc134eeafbfe36d8f4a724f545
Dr. Georg Hackenberg <7525462+ghackenberg@users.noreply.github.com>
2025-11-14 08:20:05 +0100

    add two discrete blocks

---
+++ Quellen/WS25/SFunctionHybrid/Framework/Blocks/DiscreteTimeIntegratorBlock.cs
@@ -0,0 +1,34 @@
+﻿using SFunctionContinuous.Framework;
+using SFunctionContinuous.Framework.Declarations;
+
+namespace SFunctionHybrid.Framework.Blocks
+{
+    public class DiscreteTimeIntegratorBlock : Block
+    {
+        public double StartValue { get; }
+
+        public DiscreteTimeIntegratorBlock(string name, double startValue, double sampleTimeOffset, double sampleTimePeriod) : base(name, sampleTimeOffset, sampleTimePeriod)
+        {
+            DiscreteStates.Add(new StateDeclaration("X"));
+
+            Inputs.Add(new InputDeclaration("U", false));
+
+            Outputs.Add(new OutputDeclaration("Y"));
+        }
+
+        public override void InitializeStates(double[] continuousStates, double[] discreteStates)
+        {
+            discreteStates[0] = StartValue;
+        }
+
+        public override void CalculateOutputs(double time, double[] continuousStates, double[] discreteStates, double[] inputs, double[] outputs)
+        {
+            outputs[0] = discreteStates[0];
+        }
+
+        public override void UpdateStates(double time, double[] continuousStates, double[] discreteStates, double[] inputs)
+        {
+            discreteStates[0] = inputs[0] * SampleTimePeriod;
+        }
+    }
+}
---
+++ Quellen/WS25/SFunctionHybrid/Framework/Blocks/ZeroOrderHoldBlock.cs
@@ -0,0 +1,39 @@
+﻿using SFunctionContinuous.Framework;
+using SFunctionContinuous.Framework.Declarations;
+
+namespace SFunctionHybrid.Framework.Blocks
+{
+    public class ZeroOrderHoldBlock : Block
+    {
+        public double StartValue { get; }
+
+        public ZeroOrderHoldBlock(string name, double startValue, double sampleTimeOffset, double sampleTimePeriod) : base(name, sampleTimeOffset, sampleTimePeriod)
+        {
+            StartValue = startValue;
+
+            // Discrete states
+            DiscreteStates.Add(new StateDeclaration("X"));
+
+            // Inputs
+            Inputs.Add(new InputDeclaration("U", false));
+
+            // Outputs
+            Outputs.Add(new OutputDeclaration("Y"));
+        }
+
+        public override void InitializeStates(double[] continuousStates, double[] discreteStates)
+        {
+            discreteStates[0] = StartValue;
+        }
+
+        public override void CalculateOutputs(double time, double[] continuousStates, double[] discreteStates, double[] inputs, double[] outputs)
+        {
+            outputs[0] = discreteStates[0];
+        }
+
+        public override void UpdateStates(double time, double[] continuousStates, double[] discreteStates, double[] inputs)
+        {
+            discreteStates[0] = inputs[0];
+        }
+    }
+}
