419b8f24bb6ad8418e19f5d34568c4a29029b408
Dr. Georg Hackenberg <7525462+ghackenberg@users.noreply.github.com>
2025-11-17 08:30:27 +0100

    add minimum and maximum blocks

---
+++ Quellen/WS25/SFunctionHybrid/Framework/Blocks/MaximumBlock.cs
@@ -0,0 +1,22 @@
+﻿using SFunctionContinuous.Framework;
+using SFunctionContinuous.Framework.Declarations;
+using SFunctionHybrid.Framework.SampleTimes;
+
+namespace SFunctionHybrid.Framework.Blocks
+{
+    public class MaximumBlock : Block
+    {
+        public MaximumBlock(string name) : base(name, new InheritedSampleTime())
+        {
+            Inputs.Add(new InputDeclaration("U1", true));
+            Inputs.Add(new InputDeclaration("U2", true));
+
+            Outputs.Add(new OutputDeclaration("Y"));
+        }
+
+        public override void CalculateOutputs(double time, double[] continuousStates, double[] discreteStates, double[] inputs, double[] outputs)
+        {
+            outputs[0] = Math.Max(inputs[0], inputs[1]);
+        }
+    }
+}
